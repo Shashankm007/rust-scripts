1af7c479df86c6e3270b9924f159ffa6bc0426df
Shashankm007 <44527327+Shashankm007@users.noreply.github.com>
2022-05-29 09:17:17 +0530

    autoupdate
    autoupdate

---
+++ autoupdate.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         auto update testing
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        *://*/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=stackoverflow.com
+// @grant        none
+// @downloadURL
+// @updateURL
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    console.log("hi");
+})();
